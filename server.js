b360faf993dfa241732e6e83f5fe3300787d2b27
Chumani Lalasa <chumanilalasa@gmail.com>
2024-03-11 10:33:41 +0530

    initial commit

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express()
 app.get('/',(req,res) => {
     res.send('Hello NODE API')
 })
+app.get('/blog',(req,res) => {
+    res.send('Hello Blog my name is Chumani')
+})
 app.listen(3000, () => {
     console.log('Node API app is running on port 3000')
 })
